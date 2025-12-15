7924ab8c25d94da76edeb2caca290d2ba9b97f7e
MeikH <mickael.honorine@gmail.com>
2025-12-15 12:03:01 +0100

    Make Login layout mobile-friendly

--- app/layouts/auth.vue
+++ app/layouts/auth.vue
@@ -1,10 +1,12 @@
 <template>
-    <div class="grid grid-cols-6 min-h-screen *:p-2">
-        <div class="col-span-4 bg-primary flex items-center justify-center">
-            <div class="max-w-md space-y-8 text-white">
-                <h1 class="text-4xl font-bold mb-12 text-center">adhoc copilot</h1>
+    <div class="block md:grid grid-cols-6 min-h-screen *:p-2">
+
+        <!-- LEFT BLOCK -->
+        <div class="md:col-span-4 bg-primary flex items-center justify-center">
+            <div class="max-w-md space-y-2 md:space-y-8 text-white">
+                <h1 class="text-4xl font-bold md:mb-12 text-center">adhoc copilot</h1>
                 
-                <div class="space-y-6">
+                <div class="hidden md:block space-y-6">
                     <!-- Feature 1 -->
                     <div class="flex items-center space-x-4">
                         <div class="shrink-0 w-12 h-12 bg-white/20 rounded-full flex items-center justify-center">
@@ -40,15 +42,18 @@
                 </div>
 
                 <!-- Decorative element -->
-                <div class="mt-12 text-center">
+                <div class="hidden md:block mt-12 text-center">
                     <div class="inline-block w-16 h-1 bg-white/40 rounded"></div>
                 </div>
             </div>
         </div>
-        <div class="col-span-2 bg-base-200 flex items-center justify-center">
-            <div>
+
+        <!-- RIGHT BLOCK -->
+        <div class="md:col-span-2 bg-base-200 flex items-center justify-center">
+            <div class="w-full md:w-max">
                 <slot />
             </div>
         </div>
+        
     </div>
 </template>
